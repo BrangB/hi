16d7a3339bfb109a57d43e9bbdcd09fcdbb17b83
BrangB <brangtsawmaung89@gmail.com>
2022-01-10 13:34:54 +0630

    slider

--- index.js
+++ index.js
@@ -38,3 +38,26 @@ contact.addEventListener("click" , () =>{
     slider1.style.width = "35px";
     slider2.style.width = "35px";
  } )
+
+
+ const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
